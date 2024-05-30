f59e6c38afe5fadaf08398461bedd4a53fccea96
kesharibhai84 <vishalkeshari2004@gmail.com>
2024-05-30 13:19:34 +0530

    some key words

--- src/pages/allproducts/Allproducts.jsx
+++ src/pages/allproducts/Allproducts.jsx
@@ -57,7 +57,7 @@ function Allproducts() {
                                         <div className=" flex justify-center">
                                             <button type="button" 
                                             onClick={()=> addCart(item)}
-                                            className="focus:outline-none text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm w-full  py-2">Add To Cart</button>
+                                            className="focus:outline-none text-white bg-pink-600 hover:bg-pink-700 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm w-full  py-2">See Details</button>
 
                                         </div>
                                     </div>
