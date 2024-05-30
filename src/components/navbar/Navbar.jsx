36fad63593160daef77b0ad79be423c80d5db8b3
kesharibhai84 <vishalkeshari2004@gmail.com>
2024-05-30 21:05:36 +0530

    mobile mode logo pic update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -91,7 +91,7 @@ export default function Navbar() {
                     <Link to={'/'} className="-m-2 block p-2 font-medium text-gray-900 cursor-pointer">
                       <img
                         className="inline-block w-10 h-10 rounded-full"
-                        src="https://overreacted.io/static/profile-pic-c715447ce38098828758e525a1128b87.jpg"
+                        src="logos.png"
                         alt="Dan_Abromov" />                                        </Link>
                   </div>
                 </div>
